e5b72b3df6e27f2926a4a68432c0ab5767b40424
tail12 <tdallb12@gmail.com>
2016-11-03 21:30:34 +0000

    added certification

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def logged_in?
     !!current_user
   end
+  
+  
   
   def store_location
     session[:forwarding_url] = request.url if request.get?
