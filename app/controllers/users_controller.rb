27af46ec1f079bea8aa3266b9a4b680f0b1971f1
tail12 <tdallb12@gmail.com>
2016-11-03 21:18:28 +0000

    edit page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -15,6 +16,20 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:success] = "Updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
