79f3fb62404e1e9a36971e55a5c4279411ce421a
tail12 <tdallb12@gmail.com>
2016-11-07 05:09:01 +0000

    add error 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  
+
+  # 404 Not Found 指定したページがない、アクセス権がない
+  rescue_from ActionController::RoutingError, with: :error_404
+
+  def error_404(e)
+    @exception = e
+    render 'errors/error_404', status: 404, formats: [:html]
+  end
+
   private
   def logged_in_user
     unless logged_in?
