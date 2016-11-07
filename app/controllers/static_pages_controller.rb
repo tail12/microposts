a7387a7c6224314c8ba59f5abcb673577ad09e06
tail12 <tdallb12@gmail.com>
2016-11-07 04:29:35 +0000

    merged and fixedconflict

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build if logged_in?
-<<<<<<< HEAD
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-=======
       # @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      @feed_items = current_user.feed_items.includes(:user).page(params[:page])
->>>>>>> master
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
 end
